38239a3e52b7cda91f418f967568e450abf22816
Aizaz Haider <aizazhaider720@gmail.com>
2022-12-01 21:26:45 +0500

    Ninja Trips App Without Animations Starter Template Starts Here

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
+      title: 'Flutter Animations',
+      home: Home(),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,133 +1,39 @@
-import 'dart:math';
-import 'package:animations_flutter_snt/screens/flutter_transform.dart';
-import 'package:animations_flutter_snt/screens/image_animation.dart';
-import 'package:animations_flutter_snt/screens/implicit_animations.dart';
-import 'package:animations_flutter_snt/screens/tween_animation.dart';
-import 'package:animations_flutter_snt/widgets/custom_text.dart';
+import 'package:animations_flutter_snt/shared/screenTitle.dart';
+import 'package:animations_flutter_snt/shared/tripList.dart';
 import 'package:flutter/material.dart';
-import 'flutter_curves.dart';
-import 'flutter_matrix_four.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
+class Home extends StatefulWidget {
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  HomeState createState() => HomeState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: const Text('Flutter App Design',
-            style: TextStyle(color: Colors.black)),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            leading: const CustomText(
-              '👉',
-              size: 30,
+        body: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/bg.png"),
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.topLeft),
             ),
-            title: const CustomText(
-              'Image Animation',
-              size: 18,
-            ),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => const ImageAnimation()));
-            },
-          ),
-          ListTile(
-            leading: const CustomText(
-              '👉',
-              size: 30,
-            ),
-            title: const CustomText(
-              'Implicit Animated',
-              size: 18,
-            ),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => const ImplicitAnimationsExample()));
-            },
-          ),
-          const Divider(),
-          ListTile(
-            leading: const CustomText(
-              '👉',
-              size: 30,
-            ),
-            title: const CustomText(
-              'Flutter transform',
-              size: 18,
-            ),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => const FlutterTransformExample()));
-            },
-          ),
-          const Divider(),
-          ListTile(
-            leading: const CustomText(
-              '👉',
-              size: 30,
-            ),
-            title: const CustomText(
-              'Flutter Matrix4',
-              size: 18,
-            ),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => const FlutterMatrixFourExample()));
-            },
-          ),
-          const Divider(),
-          ListTile(
-            leading: const CustomText(
-              '👉',
-              size: 30,
-            ),
-            title: const CustomText(
-              'Flutter Animation curves',
-              size: 18,
-            ),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => const FlutterCurvesExample()));
-            },
-          ),
-          const Divider(),
-          ListTile(
-            leading: const CustomText(
-              '👉',
-              size: 30,
-            ),
-            title: const CustomText(
-              'Flutter Tween Animations',
-              size: 18,
-            ),
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) => const TweenAnimationExample()));
-            },
-          ),
-        ],
-      ),
-    );
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 40),
+                const SizedBox(
+                  height: 160,
+                  child: ScreenTitle(
+                    text: 'Ninja Trips',
+                  ),
+                ),
+                Flexible(
+                  child: TripList(),
+                )
+                //Sandbox(),
+              ],
+            )));
   }
 }
