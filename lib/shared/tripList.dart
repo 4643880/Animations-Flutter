cf33305164e418ec004961bbcd2f1e4f7636909f
Aizaz Haider <aizazhaider720@gmail.com>
2022-12-04 13:09:35 +0500

    Implemented Animated List

--- lib/shared/tripList.dart
+++ lib/shared/tripList.dart
@@ -8,12 +8,16 @@ class TripList extends StatefulWidget {
 }
 
 class _TripListState extends State<TripList> {
-  List<Trip> _tripList = [];
+  final GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
+  final List<Trip> _tripList = [];
 
   @override
   void initState() {
     super.initState();
-    _addTrips();
+    // It means this method should run after built method
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      _addTrips();
+    });
   }
 
   void _addTrips() {
@@ -47,55 +51,69 @@ class _TripListState extends State<TripList> {
 
     _trips.forEach((Trip trip) {
       _tripList.add(trip);
+      // index no e.g total items in list are 10 then index will be 9 means -1
+      listKey.currentState?.insertItem(
+        _tripList.length - 1,
+        duration: const Duration(seconds: 1),
+      );
     });
   }
 
+  final Tween<Offset> _offset = Tween<Offset>(
+    begin: const Offset(1, 0),
+    end: const Offset(0, 0),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _tripList.length,
-      itemBuilder: (context, index) {
+    return AnimatedList(
+      key: listKey,
+      initialItemCount: _tripList.length,
+      itemBuilder: (context, index, animation) {
         final item = _tripList[index];
-        return ListTile(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => Details(
-                  trip: item,
+        return SlideTransition(
+          position: animation.drive(_offset),
+          child: ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Details(
+                    trip: item,
+                  ),
                 ),
-              ),
-            );
-          },
-          contentPadding: const EdgeInsets.all(25),
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('${item.nights} nights',
+              );
+            },
+            contentPadding: const EdgeInsets.all(25),
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('${item.nights} nights',
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[300])),
+                Text(
+                  item.title,
                   style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue[300])),
-              Text(
-                item.title,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.grey[600],
+                    fontSize: 20,
+                    color: Colors.grey[600],
+                  ),
                 ),
-              ),
-            ],
-          ),
-          leading: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Hero(
-              tag: "location-image-${item.img}",
-              child: Image.asset(
-                'assets/images/${item.img}',
-                height: 50.0,
+              ],
+            ),
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Hero(
+                tag: "location-image-${item.img}",
+                child: Image.asset(
+                  'assets/images/${item.img}',
+                  height: 50.0,
+                ),
               ),
             ),
+            trailing: Text('\$${item.price}'),
           ),
-          trailing: Text('\$${item.price}'),
         );
       },
     );
